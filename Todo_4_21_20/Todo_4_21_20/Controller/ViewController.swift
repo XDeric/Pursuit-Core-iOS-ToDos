8a1bc496df65f4f86df6c8763ffb3c705bdd1f25
eeeric3995@yahoo.com <ihxx2s7tbu0>
2020-04-26 14:57:06 -0400

    setting tableview up

--- Todo_4_21_20/Todo_4_21_20/Controller/ViewController.swift
+++ Todo_4_21_20/Todo_4_21_20/Controller/ViewController.swift
@@ -20,7 +20,18 @@ class ViewController: UIViewController {
             tableview.reloadData()
         }
     }
-
+    
+    lazy var add: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage.init(systemName: "plus"), for: .normal)
+        button.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(addPressed), for: .touchUpInside)
+        button.isEnabled = true
+        return button
+    }()
+    
     lazy var tableview: UITableView = {
         let tv = UITableView()
         tv.delegate = self
@@ -30,17 +41,28 @@ class ViewController: UIViewController {
         return tv
     }()
     
-    
+    @objc func addPressed(){
+        let addVC = addTaskVC()
+        present(addVC, animated: true, completion: nil)
+    }
     
     func setUp(){
         tableview.translatesAutoresizingMaskIntoConstraints = false
+        add.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(add)
         view.addSubview(tableview)
         
         NSLayoutConstraint.activate([
             tableview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableview.leftAnchor.constraint(equalTo: view.leftAnchor),
             tableview.rightAnchor.constraint(equalTo: view.rightAnchor),
-            tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            add.topAnchor.constraint(equalTo: view.topAnchor),
+            add.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            add.widthAnchor.constraint(equalToConstant: 50),
+            add.heightAnchor.constraint(equalToConstant: 30)
+            
         ])
     }
     
@@ -58,11 +80,33 @@ class ViewController: UIViewController {
 //MARK: Extensions
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        switch section {
+        case 0:
+            return outTask.count
+        case 1:
+            return compTask.count
+        default:
+            return 0
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Outstanding Task"
+        case 1:
+            return "Completed Task"
+        default:
+            return ""
+        }
     }
     
     
