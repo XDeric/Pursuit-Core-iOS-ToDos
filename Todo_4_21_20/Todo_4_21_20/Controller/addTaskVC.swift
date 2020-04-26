f4f8511d0f73f3e62d5885f5a301660acf4483f9
eeeric3995@yahoo.com <ihxx2s7tbu0>
2020-04-26 13:12:00 -0400

    organized files

---
+++ Todo_4_21_20/Todo_4_21_20/Controller/addTaskVC.swift
@@ -0,0 +1,30 @@
+//
+//  addTaskVC.swift
+//  Todo_4_21_20
+//
+//  Created by EricM on 4/26/20.
+//  Copyright © 2020 EricM. All rights reserved.
+//
+
+import UIKit
+
+class addTaskVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
